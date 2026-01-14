df2e64ef6f740caaabb0e6efa42b8079d9461e61
samuel-p-fox <samuel.paul.fox@gmail.com>
2026-01-14 17:53:25 +0100

    Refactor main.py for improved readability

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ HeroStrength = 20
 WHITE = (255, 255,255)
 BLACK = (0, 0, 0)
 GREY = (125, 125, 125)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-CYAN = (0, 255, 255)
-MAGENTA = (255, 0, 255)
 
 # Frames per Second
 FPS = 45
@@ -73,5 +67,6 @@ while loop_variable == True:
 pygame.quit()
 
 sys.exit()
+
 
 
