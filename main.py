42df3d2e808ce2cd8bb651b5979ae34b274bb613
samuel-p-fox <samuel.paul.fox@gmail.com>
2026-01-15 14:23:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 screen.fill((40, 40, 55))
 clock = pygame.time.Clock()
 
+WIDTH = screen.get_width()
+HEIGHT = screen.get_height()
+state = "menu"
 player_speed = 5
 GRAVITY = 0.5
 velocity = 0
@@ -14,12 +17,32 @@ jump_power = -10
 on_ground = True
 
 # Créer les plateformes
-plateformes = [pygame.Rect(0,1050,2000,25), pygame.Rect(120,450,60,25), pygame.Rect(230,375,50,40), pygame.Rect(345,250,80,25), pygame.Rect(460,150,30,25), pygame.Rect(0,0,10,2000),]
+plateformes = [
+    pygame.Rect(0, HEIGHT - 25, WIDTH, 25),
+    pygame.Rect(120, 450, 80, 25),
+    pygame.Rect(260, 350, 80, 25),
+    pygame.Rect(400, 250, 80, 25),
+    pygame.Rect(550, 150, 80, 25),
+]
 
-perso1_image = pygame.image.load(r"U:\Tower of Heights\épéiste_couleur.png").convert_alpha()
-perso1_rect = perso1_image.get_rect(topleft=(200,300))
+# Pour appeler les images
+perso1_image = pygame.image.load("silhouette_épéiste.png").convert_alpha()
+perso2_image = pygame.image.load("épéiste_couleur.png").convert_alpha()
 
-alive = True # Variable du jeu
+perso1_rect_menu = perso1_image.get_rect(center=(WIDTH//2 - 150, HEIGHT//2))
+perso2_rect_menu = perso2_image.get_rect(center=(WIDTH//2 + 150, HEIGHT//2))
+
+selected_image = None
+perso_rect = None
+
+# Pour le texte
+title_font = pygame.font.SysFont(None, 100)
+text_font = pygame.font.SysFont(None, 40)
+
+title_surface = title_font.render("Tower of Heights", True, (240, 240, 240))
+title_rect = title_surface.get_rect(center=(WIDTH//2, 120))
+
+runing = True # Variable du jeu
 
 # Boucle principale
 while running == True:
@@ -31,21 +54,44 @@ while running == True:
             
     key = pygame.key.get_pressed() # Pour quand on appuie sur une touche
 
+    # Pour donner le choix de personnages sur la page menu de départ
+    if state == "menu" and event.type == pygame.MOUSEBUTTONDOWN:
+            if perso1_rect_menu.collidepoint(event.pos):
+                selected_image = perso1_image
+                perso_rect = selected_image.get_rect(topleft=(200, 300))
+                state = "game"
+
+            if perso2_rect_menu.collidepoint(event.pos):
+                selected_image = perso2_image
+                perso_rect = selected_image.get_rect(topleft=(200, 300))
+                state = "game"
+
+    # Pour créer la page menu de départ
+    if state == "menu":
+        screen.fill((30, 30, 45))
+        screen.blit(title_surface, title_rect)
+
+        screen.blit(perso1_image, perso1_rect_menu)
+        screen.blit(perso2_image, perso2_rect_menu)
+
+        txt = text_font.render("Clique sur ton personnage", True, (200, 200, 200))
+        screen.blit(txt, (WIDTH//2 - txt.get_width()//2, HEIGHT - 120))
+
+        pygame.display.flip() # Pour mettre la fenêtre à jour
+        continue
     
     # Pour bouger
-    if key[pygame.K_LEFT]:
+    if key[pygame.K_LEFT]: # Aller à gauche
         perso1_rect.x -= player_speed
-    if key[pygame.K_RIGHT]:
+    if key[pygame.K_RIGHT]: # Aller à droite
         perso1_rect.x += player_speed
-
-    # Pour sauter
-    if key[pygame.K_SPACE] and on_ground:
+    if key[pygame.K_SPACE] and on_ground: # Saut
         velocity += jump_power
         on_ground = False
 
-    # Pour redecendre grâce à la gravity
+    # Pour redecendre grâce à la gravité
     if not on_ground:
-        velocity += gravity
+        velocity += GRAVITY
 
     # Pour rester sur les plateformes
     for plateforme in plateformes:
@@ -62,11 +108,11 @@ while running == True:
     screen.fill((40, 40, 55)) # Pour remplir la fenêtre
 
     # Pour générer les plateformes
-    for plateforme in plateformes: pygame.draw.rect(screen,(50, 20, 20), (plateforme.x, plateforme.y, plateforme.width, plateforme.height))
+    for plateforme in plateformes: pygame.draw.rect(screen, (120, 60, 60), plateforme)
 
 
 
-    screen.blit(perso1_image, (perso1_rect.x, perso1_rect.y)) # Pour générer le personnage
+    screen.blit(selected_image, perso_rect) # Pour générer le personnage
 
     pygame.display.flip() # Pour mettre l'ensemble de la fenêtre à jour
 
