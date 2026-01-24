97d965f801fadf09ed9b74455671a76999a4ac97
samuel-p-fox <samuel.paul.fox@gmail.com>
2026-01-24 16:53:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import pygame # Importer la bibliothèque
-pygame.init() # Initialiser pygame
+
+# Initialier pygame
+pygame.init()
 pygame.mixer.init()
 
 pygame.display.set_caption("Tower of Heights") # Quand la fenêtre est ouverte, afficher "Tower of Heights"
+
+# Pour le son de fond
 pygame.mixer.music.load("MusiqueDebase.mp3")
 pygame.mixer.music.set_volume(1)
 
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #définit la taille de la fenêtre (plein écran)
-screen.fill((40, 40, 55)) #couleur de l'écran 
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN) #definit la taille de la fenêtre (plein ecran)
+screen.fill((40, 40, 55)) # Couleur de l'ecran 
 clock = pygame.time.Clock()
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 
-WIDTH = screen.get_width() #constante = largeur de l'écran
-HEIGHT = screen.get_height() #constante = hauteur de l'écran
-state = "menu_de_début" #le jeu démare sur la fenêtre de menu
+# Constante pour la fenêtre
+WIDTH = screen.get_width() #constante = largeur de l'ecran
+HEIGHT = screen.get_height() #constante = hauteur de l'ecran
+
+state = "menu_de_debut" #le jeu demare sur la fenêtre de menu
+
+# Variables de mouvement
 player_speed = 5 #vitesse du joueur
 GRAVITY = 0.4 #vitesse de chute
 velocity = 0 #variable = vitesse de saut - vitesse de chute
 jump_power = -15 #puisssance de saut
 on_ground = False #contact avec le sol
-monster_speed = 2 #vitesse de déplacement des monstres
-life = 0
-pushback = 50
+monster_speed = 2 #vitesse de deplacement des monstres
 
-# Créer les plateformes
+life = 5 # Nombres de vies de départ
+PUSHBACK = 100
+
+# --- Plateformes ---
 plateforms = [
     pygame.Rect(0, HEIGHT - 25, WIDTH, 25),
     pygame.Rect(120, 450, 80, 25),
@@ -34,11 +47,11 @@ plateforms = [
     pygame.Rect(550, 150, 80, 25),
 ]
 
-# Pour appeler les images
-    # Héros
+# --- Images ---
+
+    # Heros
 perso1_image = pygame.image.load("silhouette_épéiste.png").convert_alpha()
 perso2_image = pygame.image.load("épéiste_couleur.png").convert_alpha()
-perso1_image_attaque = pygame.image.load("épéiste_attaque.png").convert_alpha()
 
     # Monstre
 monster = pygame.transform.scale(pygame.image.load("slug.png").convert_alpha(), (150, 112.5))
@@ -49,52 +62,32 @@ monster_rect = monster.get_rect(topleft = (0, HEIGHT - 130))
 # Prend le rect des images
 perso1_rect_menu = perso1_image.get_rect(center=(WIDTH//2 - 150, HEIGHT//2))
 perso2_rect_menu = perso2_image.get_rect(center=(WIDTH//2 + 150, HEIGHT//2))
-perso1_attaque_rect = perso1_image_attaque.get_rect(center=(WIDTH//2 - 150, HEIGHT//2))
 
-selected_image = None #image sélectioné, non-définie pour l'instant
-image_attack = perso1_image_attaque
+selected_image = None # Image selectionnée, non-definie pour l'instant
 image_rect_attack = perso1_rect_menu
-perso_rect = None #rect de l'image sélectionné
+perso_rect = None # Rect de l'image selectionnée
 
-# Boutons écran de mort
+# --- Boutons écran de mort ---
+    # Celui pour recommencer
 restart_rect_death = pygame.Rect(0, 255, 200, 60)
 restart_rect_death.center = (WIDTH//2 - 150, HEIGHT//2 + 120)
 
+    # Celui pour arrêter
 end_rect_death = pygame.Rect(255, 0, 200, 60)
 end_rect_death.center = (WIDTH//2 + 150, HEIGHT//2 + 120)
 
-# Pour le texte
-title_font = pygame.font.SysFont(None, 100) #caractéristiques du titre
-text_font = pygame.font.SysFont(None, 40) #caractéristiques du texte
-death_txt_font = pygame.font.SysFont("youmurdererbb_reg.ttf", 64) #caractéristiques du texte de mort
+# --- Variables de texte ---
+title_font = pygame.font.SysFont(None, 100) # Caracteristiques du titre
+text_font = pygame.font.SysFont(None, 40) # Caracteristiques du texte
+death_txt_font = pygame.font.SysFont("you-murderer.zip/youmurdererbb_reg.ttf", 64) # Caracteristiques du texte de mort
 
 title_surface = title_font.render("Tower of Heights", True, (240, 240, 240)) 
 title_rect = title_surface.get_rect(center=(WIDTH//2, 120))
 
-def menu_de_début():
-    global state, perso1_rect_menu, perso1_image, selected_image, selected_image_left, selected_image_right, perso_rect, selected_attack_left,  selected_attack_right
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if perso1_rect_menu.collidepoint(event.pos):
-            selected_image = perso1_image
-            selected_image_right = selected_image
-            selected_image_left = pygame.transform.flip(selected_image, True, False)
-            selected_attack = pygame.image.load("épéiste_attaque.png").convert_alpha()
-            selected_attack_right = selected_attack
-            selected_attack_left = pygame.transform.flip(selected_attack, True, False)
-            perso_rect = selected_image.get_rect(topleft=(200, 300))
-            state = "game"
-            pygame.mixer.music.play(-1)
-
-        if perso2_rect_menu.collidepoint(event.pos):
-            selected_image = perso2_image
-            selected_image_right = selected_image
-            selected_image_left = pygame.transform.flip(selected_image, True, False)
-            selected_attack = pygame.image.load("épéiste_attaque.png").convert_alpha()
-            selected_attack_right = selected_attack
-            selected_attack_left = pygame.transform.flip(selected_attack, True, False)
-            perso_rect = selected_image.get_rect(topleft=(200, 300))
-            state = "game"
-            pygame.mixer.music.play(-1)
+# --- Lorsqu'échape est cliqué ---
+pause_box = pygame.Rect(WIDTH//2 - 250, HEIGHT//2 - 150, 500, 300)
+continue_button = pygame.Rect(WIDTH//2 - 200, HEIGHT//2 + 40, 180, 60)
+quit_button = pygame.Rect(WIDTH//2 + 20, HEIGHT//2 + 40, 180, 60)
 
 running = True # Variable du jeu
 
@@ -106,22 +99,77 @@ while running:
 
     # Pour sortir de la fenêtre de jeu
     for event in pygame.event.get():
-        # Print text saying "do you want to stop?" then buttons with options yes and no
-        if event.type == pygame.QUIT or key[pygame.K_ESCAPE]: running = False
+        if event.type == pygame.QUIT or state != "game" and event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
 
-        # Pour donner le choix de personnages sur la page menu de départ
-        if state == "menu_de_début":
-            menu_de_début()
-        
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE and state == "game":
+            state = "paused"
+            pygame.mixer.music.pause() # Arrêter la musique
+
+        # --- Boutons de pause ---
+        if state == "paused" and event.type == pygame.MOUSEBUTTONDOWN:
+            if continue_button.collidepoint(event.pos): # Si on appuie sur le bouton pour continuer
+                state = "game"
+                pygame.mixer.music.unpause() # Continuer la musique
+            if quit_button.collidepoint(event.pos): # Si on appuie sur le bouton pour quitter
+                running = False
+
+        # Pour donner le choix de personnages sur la page menu de depart
+        if state == "menu_de_debut" and event.type == pygame.MOUSEBUTTONDOWN:
+            if perso1_rect_menu.collidepoint(event.pos):
+                selected_image = perso1_image
+                selected_image_right = selected_image
+                selected_image_left = pygame.transform.flip(selected_image, True, False)
+                selected_attack = pygame.image.load("épéiste_attaque.png").convert_alpha()
+                selected_attack_right = selected_attack
+                selected_attack_left = pygame.transform.flip(selected_attack, True, False)
+                perso_rect = selected_image.get_rect(topleft=(200, 300))
+                state = "game"
+                pygame.mixer.music.play(-1)
+
+            if perso2_rect_menu.collidepoint(event.pos):
+                selected_image = perso2_image
+                selected_image_right = selected_image
+                selected_image_left = pygame.transform.flip(selected_image, True, False)
+                selected_attack = pygame.image.load("épéiste_attaque.png").convert_alpha()
+                selected_attack_right = selected_attack
+                selected_attack_left = pygame.transform.flip(selected_attack, True, False)
+                perso_rect = selected_image.get_rect(topleft=(200, 300))
+                state = "game"
+                pygame.mixer.music.play(-1)
+
         # Pour la page de mort
         if state == "death" and event.type == pygame.MOUSEBUTTONDOWN:
             if restart_rect_death.collidepoint(event.pos):
-                state = "menu_de_début"
+                state = "menu_de_debut"
             elif end_rect_death.collidepoint(event.pos):
                 state = "end"
 
-    # Pour créer la page du menu de départ
-    if state == "menu_de_début":
+    # --- Pause ---
+    if state == "paused":
+        # Dessiner le rectangle de pause
+        pygame.draw.rect(screen, WHITE, pause_box)
+        pygame.draw.rect(screen, BLACK, pause_box, 3)
+
+        # Pour poser la question
+        txt = text_font.render("Que veux-tu faire ?", True, BLACK)
+        screen.blit(txt, (pause_box.x + 100, pause_box.y + 40))
+
+        # Bouton pour continuer
+        pygame.draw.rect(screen, GREEN, continue_button) # Pour dessiner un rectangle vert...
+        pygame.draw.rect(screen, BLACK, continue_button, 2) # ...et sa bordure noire
+        screen.blit(text_font.render("Continuer", True, BLACK), (continue_button.x + 20, continue_button.y + 15)) # Pour afficher le texte
+
+        # Bouton pour arrêter
+        pygame.draw.rect(screen, RED, quit_button) # Pour dessiner un rectangle rouge...
+        pygame.draw.rect(screen, BLACK, quit_button, 2) # ...et sa bordure noire
+        screen.blit(text_font.render("Quitter", True, BLACK), (quit_button.x + 40, quit_button.y + 15)) # Pour afficher le texte
+
+        pygame.display.flip()
+        continue  # <-- FREEZE GAME HERE
+
+    # Pour creer la page du menu de depart
+    if state == "menu_de_debut":
         screen.fill((30, 30, 45))
         screen.blit(title_surface, title_rect)
 
@@ -132,22 +180,26 @@ while running:
         info = text_font.render("Appuie sur M pour revenir sur cette page", True, (200, 200, 200))
         screen.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT - 120))
         screen.blit(info, (WIDTH//2 - info.get_width()//2, HEIGHT - 80))
-        life = 0
-        pygame.display.flip() # Pour mettre la fenêtre à jour
+        pygame.display.flip()
         continue
 
     # Pour jouer
     if state == "game":
-    # Pour bouger
-        if key[pygame.K_LEFT]: # Aller à gauche
-            perso_rect.x += -player_speed
-            selected_image = selected_image_left
+        if key[pygame.K_LEFT]:
+            perso_rect.x -= player_speed
+            if key[pygame.K_d]:
+                selected_image = selected_attack_left
+            else:
+                selected_image = selected_image_left
 
-        if key[pygame.K_RIGHT]: # Aller à droite
+        if key[pygame.K_RIGHT]:
             perso_rect.x += player_speed
-            selected_image = selected_image_right
+            if key[pygame.K_d]:
+                selected_image = selected_attack_right
+            else:
+                selected_image = selected_image_right
 
-        if key[pygame.K_SPACE] and on_ground: # Saut
+        if key[pygame.K_SPACE] and on_ground:
             velocity += jump_power
             on_ground = False
 
@@ -157,34 +209,31 @@ while running:
         if monster_rect.x < perso_rect.x:
             monster_rect.x += monster_speed
             monster = monster_right
-        
-        
-        if key[pygame.K_d]:
-            selected_image = image_attack
 
-        # Pour redecendre grâce à la gravité
         if not on_ground:
             velocity += GRAVITY
 
-        # Pour rester sur les plateformes
         for plateform in plateforms:
             if perso_rect.colliderect(plateform) and velocity > 0:
                 perso_rect.bottom = plateform.top
                 on_ground = True
                 velocity = 0
                 break
+
         if perso_rect.colliderect(monster_rect):
-            life += 1
+            life -= 1
             if perso_rect.x < monster_rect.x - 20:
-                perso_rect.x -= pushback
+                perso_rect.x -= PUSHBACK
             elif perso_rect.x > monster_rect.x + 20:
-                perso_rect.x += pushback
+                perso_rect.x += PUSHBACK
             if perso_rect.y < monster_rect.y:
                 velocity -= 2*velocity
-        if life > 5:
+
+        if life == 0:
             state = "death"
-        # Pour revenir au menu de départ
-        if key[pygame.K_m]: state = "menu_de_début"
+
+        if key[pygame.K_m]:
+            state = "menu_de_debut"
 
         velocity += GRAVITY
         perso_rect.y += velocity
@@ -196,54 +245,48 @@ while running:
                 on_ground = True
                 velocity = 0
                 break
-        
-        # Mort si chute hors écran
+
         if perso_rect.top > HEIGHT:
             state = "death"
-    
-    # Pour générer l'écran de mort
+
+    # Pour generer l'ecran de mort
     if state == "death":
         screen.fill((0, 0, 0))
         pygame.mixer.music.stop()
 
-        txt = death_txt_font.render("Bienvenue au Royaume des Défunts", True, (150, 20, 40))
+        txt = death_txt_font.render("Bienvenue au Royaume des Defunts", True, (150, 20, 40))
         screen.blit(txt, txt.get_rect(center=(WIDTH//2, HEIGHT//2 - 100)))
 
-        # Bouton REJOUER
         pygame.draw.rect(screen, (200, 0, 0), restart_rect_death)
         txt_restart = text_font.render("Rejouer", True, WHITE)
         screen.blit(txt_restart, txt_restart.get_rect(center=restart_rect_death.center))
 
-        # Bouton QUITTER
         pygame.draw.rect(screen, (0, 0, 200), end_rect_death)
         txt_end = text_font.render("Quitter", True, WHITE)
         screen.blit(txt_end, txt_end.get_rect(center=end_rect_death.center))
-        life = 0
+        life = 5
         pygame.display.flip()
         continue
 
-    # Pour la page de mort
     if state == "death" and event.type == pygame.MOUSEBUTTONDOWN:
         if restart_rect_death.collidepoint(event.pos):
-            state = "menu_de_début"
+            state = "menu_de_debut"
         elif end_rect_death.collidepoint(event.pos):
             state = "end"
 
-    # END
     if state == "end":
         screen.fill((0, 0, 100))
-        txt = text_font.render("Merci d'avoir joué à Tower Of Heights", True, WHITE)
+        txt = text_font.render("Merci d'avoir joue à Tower Of Heights", True, WHITE)
         screen.blit(txt, txt.get_rect(center=(WIDTH//2, HEIGHT//2)))
         pygame.display.flip()
-        pygame. time. wait(3000)
+        pygame.time.wait(2500)
         running = False
 
-
-    screen.fill((40, 40, 55)) # Pour remplir la fenêtre
-    for plateform in plateforms: pygame.draw.rect(screen, (120, 60, 60), plateform) # Pour générer les plateformes
-    screen.blit(selected_image, perso_rect) # Pour générer le personnage
+    screen.fill((40, 40, 55))
+    for plateform in plateforms:
+        pygame.draw.rect(screen, (120, 60, 60), plateform)
+    screen.blit(selected_image, perso_rect)
     screen.blit(monster, monster_rect)
-    pygame.display.flip() # Pour mettre l'ensemble de la fenêtre à jour
+    pygame.display.flip()
 
-
-pygame.quit() # Pour arrêter le jeu
+pygame.quit()
