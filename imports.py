18fa562a1c8256f46997e433ea34dbe1aba547fa
william-fraysse <wh.fraysse@gmail.com>
2026-03-27 15:12:29 +0100

    Update imports.py

--- imports.py
+++ imports.py
@@ -66,7 +66,7 @@ jumping_slime3 = pygame.image.load("Images/Monsters/Slime/jumping_slime3.png").c
 jumping_slime4 = pygame.image.load("Images/Monsters/Slime/jumping_slime4.png").convert_alpha()
 
         #Mushroom
-#mushroom = pygame.image.load("Images/Monsters/mushroom.png").convert_alpha()
+mushroom = pygame.image.load("Images/Monsters/mushroom.png").convert_alpha()
 
 
     # Objecs
