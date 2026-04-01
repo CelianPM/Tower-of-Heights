dd0bdba77f4051d9e4d2d00e06c3008a67a14df0
samuel-p-fox <samuel.paul.fox@gmail.com>
2026-04-01 19:37:20 +0200

    added stuff to fix the wall jump, but it isn't great yet. to be ocntinued

--- functions.py
+++ functions.py
@@ -157,7 +157,7 @@ def beginning_menu__displayer(screen, title_surface, title_rect, archer_image, a
 def game(velocity, state, monsters, arrows, camera_y, time, key, start_time, player, inventory_list, items, slot_hold_start, slot_use_lock, last_inventory_feedback, last_inventory_feedback_time, pickup_pressed, platforms, shurikens, hazards):
     """S'occupe de gerer les mouvements du joueur, les attaques, les collisions avec les plateformes et les monstres, et la mort du joueur"""
     
-    velocity, start_time = player.move(imports.jump_sound, state, time, key, velocity, start_time, arrows, shurikens)
+    velocity, start_time = player.move(imports.jump_sound, state, time, key, velocity, start_time, arrows, shurikens, platforms)
     velocity = player.platform_collisions(platforms, velocity)
     player.monster_collisions(monsters, time, arrows, platforms, shurikens)
     player.hazard_collisions(hazards, time)
